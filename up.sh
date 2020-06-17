baa50b746ecf5ed98e3513e58532975b4b07b00f
Nic Dobbins <nicdobbins@Nics-MacBook-Pro.local>
2020-06-16 21:49:56 -0700

    Added sentence caching

--- up.sh
+++ up.sh
@@ -34,7 +34,7 @@ def wait_till_up(name):
                 sys.exit()
 
 def main():
-    images = get_images()
+    images = [ img for key, img in get_images().items() ]
     containers = [ container for key, container in get_containers().items() ]
     env_vars = get_env_vars()
     mmlite_inst_cnt = int(sys.argv[1]) if len(sys.argv) > 1 and sys.argv[1].isdigit() else 1
@@ -55,7 +55,7 @@ def main():
     img_name = f'{APP_NAME}_{OPEN_NLP}'
     port = env_vars[ENV_OPENNLP_PORT]
 
-    if not images.get(img_name):
+    if not any([ x for x in images if img_name in x.name ]):
         build(img_name, OPEN_NLP)
     if not any([ x for x in containers if cont_name in x.name ]):
         run(cont_name, img_name, port)
@@ -65,7 +65,7 @@ def main():
     img_name = f'{APP_NAME}_{METAMAP}'
     port = int(env_vars[ENV_METAMAP_PORT])
 
-    if not images.get(img_name):
+    if not any([ x for x in images if img_name in x.name ]):
         build(img_name, METAMAP)
     
     for i in range(mmlite_inst_cnt):
@@ -76,8 +76,12 @@ def main():
         port += 1
 
     # Clear dangling PIDs
-    cmd = "ps x | grep './up.sh' | awk '{print $1}' | xargs kill -9"
-    run_shell_cmd(cmd)
+    #cmd = "ps x | grep './up.sh' | awk '{print $1}' | xargs kill -9"
+    #run_shell_cmd(cmd)
+    pids = [ l.strip().split(' ')[0] for l in str(run_shell_cmd('ps x')[0]).split('\\n') if './up.sh' in l and 'python3' in l ]
+    for p in pids:
+        print(f'kill -9 {p}')
+        run_shell_cmd(f'kill -9 {p}')
 
 if __name__ == '__main__':
     main()
